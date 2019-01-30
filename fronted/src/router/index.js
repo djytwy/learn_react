cb47c313388c1417e2591c9d693dd6ee9165772c
twy <676534074@qq.com>
2019-01-30 19:46:14 +0800

    重构项目(待完成)

--- fronted/src/router/index.js
+++ fronted/src/router/index.js
@@ -2,8 +2,37 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/components/login'
 import home from '@/pages/home'
+import store from '../store/index.js'
 
 Vue.use(Router)
+
+// 路由懒加载
+const Home = resolve => {
+  require.ensure(['../components/home.vue'], () => {
+    resolve(require('../components/home.vue'))
+  })
+}
+
+const Login 
+
+const router = new Router({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      name:'home',
+      component:Home,
+      meta:{ 
+        requireAuth: true
+      }
+    },
+    {
+      path:'login',
+      name:'login',
+    }
+  ]
+})
+
 
 export default new Router({
   routes: [
