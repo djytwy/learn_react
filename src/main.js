ee1695ee191549856e9898fbb907c48de41630c5
twy <676534074@qq.com>
2019-03-13 18:27:30 +0800

    使用cdnboot外链，使vendor文件变小

--- src/main.js
+++ src/main.js
@@ -1,27 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
+// import Vue from 'vue';
 import App from './App';
 import router from './router';
 import 'element-ui/lib/theme-chalk/index.css';
-import {Button,Row,Input,Dialog,MessageBox,Form,FormItem,Message} from 'element-ui';
-import axios from 'axios';
+
+// import axios from 'axios';
 import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(Button);
-Vue.use(Row);
-Vue.use(Input);
-Vue.use(Dialog);
-Vue.use(Form);
-Vue.use(FormItem);
+
+// 部署生产环境时候，这里可以注释掉，在webpack里面external掉 ElementUI、Vue等相应的部分，可大大减小vendor文件的大小
+// import ElementUI from 'element-ui';
+// import Vue from 'vue'
+// Vue.use(ElementUI)
 
 Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
-Vue.prototype.$confirm = MessageBox.confirm;
 Vue.prototype.$http = axios;
-Vue.prototype.$message = Message;
 
-Vue.prototype.$http = axios
 axios.defaults.withCredentials=true;
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import Router from 'vue-router'
 import store from '../store/index.js'
 
