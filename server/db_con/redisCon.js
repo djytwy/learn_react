51873036ce5806ad72d93e930ca870835010541a
twy <676534074@qq.com>
2019-04-01 20:30:05 +0800

    更新到Vue-cli 3.0

--- server/db_con/redisCon.js
+++
@@ -1,6 +0,0 @@
-const Redis = require('ioredis');
-const conf = require('./../config')
-
-redis = new Redis(conf.REDIS.PORT,conf.REDIS.DB_URL);
-
-module.exports = redis;
--- server/routers/home.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router')
-const Home = require('./../controllers/home')
-
-const router = new Router()
-
-module.exports = router
-    .get('',Home.home)
-    .get('login',Home.home)
-
