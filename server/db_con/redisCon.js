ad15267c1454db3e41b50782bc67bc84ddbc9a39
djytwy <676534074@qq.com>
2019-12-25 00:11:31 +0800

    使用react重构前端

--- server/db_con/redisCon.js
+++
@@ -1,6 +0,0 @@
-const Redis = require('ioredis')
-const conf = require('./../config')
-
-const redis = new Redis(conf.REDIS.PORT,conf.REDIS.DB_URL);
-
-module.exports = redis;
--- server/routers/test.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router')
-const Test = require('./../controllers/test')
-
-const router = new Router()
-
-module.exports = router.
-    get('/test',Test.io_test)
-
